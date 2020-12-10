d8ba1d0f66cf3f8b7711b2e4b319faf3310d9af0
Marco La Martina <marco.lamartina@community.unipa.it>
2020-12-10 22:09:29 +0100

    code simplified

--- catkin_ws/src/webots_ros/src/change.cpp
+++ catkin_ws/src/webots_ros/src/change.cpp
@@ -26,35 +26,24 @@ ros::ServiceClient timeStepClient;
 webots_ros::set_int timeStepSrv;
 
 static const char *motorNames[NMOTORS] = {"left_wheel_motor", "right_wheel_motor"};
+const std::string name = "change";
 
-static double GPSValues[3] = {0, 0, 0};
-static double inertialUnitValues[4] = {0, 0, 0, 0};
-
-static int lms291Resolution = 0;
-static int halfResolution = 0;
-static double maxRange = 0.0;
-static double rangeThreshold = 0.0;
-static std::vector<double> braitenbergCoefficients;
-static bool areBraitenbergCoefficientsinitialized = false;
 
 // gaussian function
 double gaussian(double x, double mu, double sigma) {
   return (1.0 / (sigma * sqrt(2.0 * M_PI))) * exp(-((x - mu) * (x - mu)) / (2 * sigma * sigma));
 }
 
-void updateSpeed() {
-  // init dynamic variables
-  double leftObstacle = 0.0, rightObstacle = 0.0, obstacle = 0.0;
+void maxSpeed() {
+  // init variables
   double speeds[NMOTORS];
-  // apply the braitenberg coefficients on the resulted values of the lms291
-  // near obstacle sensed on the left side
-  speeds[0] = MAX_SPEED;
-  speeds[1] = MAX_SPEED;
+
   // set speeds
   for (int i = 0; i < NMOTORS; ++i) {
+    speeds[i] = MAX_SPEED;
     ros::ServiceClient set_velocity_client;
     webots_ros::set_float set_velocity_srv;
-	set_velocity_client = n->serviceClient<webots_ros::set_float>(std::string("change/") + std::string(motorNames[i]) + std::string("/set_velocity"));
+	  set_velocity_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motorNames[i]) + std::string("/set_velocity"));
     set_velocity_srv.request.value = speeds[i];
     set_velocity_client.call(set_velocity_srv);
   }
@@ -73,7 +62,7 @@ void controllerNameCallback(const std_msgs::String::ConstPtr &name) {
 }
 
 void quit(int sig) {
-  ROS_INFO("User stopped the 'change' node.");
+  ROS_INFO("User stopped the "+name+" node.");
   timeStepSrv.request.value = 0;
   timeStepClient.call(timeStepSrv);
   ros::shutdown();
@@ -82,8 +71,8 @@ void quit(int sig) {
 
 int main(int argc, char **argv) {
   std::string controllerName;
-  // create a node named 'change' on ROS network
-  ros::init(argc, argv, "change", ros::init_options::AnonymousName);
+  // create a node named as "name" variable on ROS network
+  ros::init(argc, argv, name, ros::init_options::AnonymousName);
   n = new ros::NodeHandle;
 
   signal(SIGINT, quit);
@@ -97,7 +86,7 @@ int main(int argc, char **argv) {
   }
   ros::spinOnce();
 
-  timeStepClient = n->serviceClient<webots_ros::set_int>("change/robot/time_step");
+  timeStepClient = n->serviceClient<webots_ros::set_int>(name+"/robot/time_step");
   timeStepSrv.request.value = TIME_STEP;
 
   // if there is more than one controller available, it let the user choose
@@ -123,7 +112,7 @@ int main(int argc, char **argv) {
     // position
     ros::ServiceClient set_position_client;
     webots_ros::set_float set_position_srv;
-    set_position_client = n->serviceClient<webots_ros::set_float>(std::string("change/") + std::string(motorNames[i]) +
+    set_position_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motorNames[i]) +
                                                                   std::string("/set_position"));
 
     set_position_srv.request.value = INFINITY;
@@ -137,7 +126,7 @@ int main(int argc, char **argv) {
     // speed
     ros::ServiceClient set_velocity_client;
     webots_ros::set_float set_velocity_srv;
-    set_velocity_client = n->serviceClient<webots_ros::set_float>(std::string("change/") + std::string(motorNames[i]) +
+    set_velocity_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motorNames[i]) +
                                                                   std::string("/set_velocity"));
 
     set_velocity_srv.request.value = 0.0;
@@ -147,21 +136,45 @@ int main(int argc, char **argv) {
       ROS_ERROR("Failed to call service set_velocity on motor %s.", motorNames[i]);
   }
 
+  // enable accelerometer
+  ros::ServiceClient set_accelerometer_client;
+  webots_ros::set_int accelerometer_srv;
+  ros::Subscriber sub_accelerometer;
+  set_accelerometer_client = n->serviceClient<webots_ros::set_int>(name+"/accelerometer/enable");
+  accelerometer_srv.request.value = 32;
+  set_accelerometer_client.call(accelerometer_srv);
+
   // enable camera
   ros::ServiceClient set_camera_client;
   webots_ros::set_int camera_srv;
   ros::Subscriber sub_camera;
-  set_camera_client = n->serviceClient<webots_ros::set_int>("change/camera/enable");
+  set_camera_client = n->serviceClient<webots_ros::set_int>(name+"/camera/enable");
   camera_srv.request.value = 64;
   set_camera_client.call(camera_srv);
 
-  ROS_INFO("You can now start the creation of the map using 'rosrun gmapping slam_gmapping "
-           "scan:=/change/Sick_LMS_291/laser_scan/layer0 _xmax:=30 _xmin:=-30 _ymax:=30 _ymin:=-30 _delta:=0.2'.");
+  // enable gyro
+  ros::ServiceClient set_gyro_client;
+  webots_ros::set_int gyro_srv;
+  ros::Subscriber sub_gyro;
+  set_gyro_client = n->serviceClient<webots_ros::set_int>(name+"/gyro/enable");
+  gyro_srv.request.value = 32;
+  set_gyro_client.call(gyro_srv);
+
+
+  // enable servo
+  ros::ServiceClient set_gyro_client;
+  webots_ros::set_int gyro_srv;
+  ros::Subscriber sub_gyro;
+  set_gyro_client = n->serviceClient<webots_ros::set_int>(name+"/gyro/enable");
+  gyro_srv.request.value = 32;
+  set_gyro_client.call(gyro_srv);
+
   ROS_INFO("You can now visualize the sensors output in rqt using 'rqt'.");
 
+  // for test
+  maxSpeed();
   // main loop
   while (ros::ok()) {
-	updateSpeed();
     if (!timeStepClient.call(timeStepSrv) || !timeStepSrv.response.success) {
       ROS_ERROR("Failed to call service time_step for next step.");
       break;
