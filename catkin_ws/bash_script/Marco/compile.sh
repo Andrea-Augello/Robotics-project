39c1bf2b09f6ad2ba82356be8095210ae31b8e62
Marco <marco.lamartina@community.unipa.it>
2020-12-10 09:35:30 +0100

    bash script in separated directory

---
+++ catkin_ws/bash_script/Marco/compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/Github/Robotics-project/catkin_ws;
+source devel/setup.bash;
+catkin_make;
+
---
+++ catkin_ws/bash_script/Marco/launch.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/Github/Robotics-project/catkin_ws;
+source devel/setup.bash;
+roslaunch robotics_project change.launch;
