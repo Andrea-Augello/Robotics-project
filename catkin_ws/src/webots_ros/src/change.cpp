4c412b5317ebbfd741cca409ec34ecadc2a8d553
Marco La Martina <marco.lamartina@community.unipa.it>
2020-12-12 12:59:54 +0100

    spin_servo and speak_polyglot added, camel case removed

--- catkin_ws/src/webots_ros/src/change.cpp
+++ catkin_ws/src/webots_ros/src/change.cpp
@@ -16,7 +16,8 @@
 #include <webots_ros/display_image_paste.h>
 
 #define TIME_STEP 32
-#define NMOTORS 2
+#define N_MOTORS 2
+#define N_LANGUAGES 6
 #define MAX_SPEED 6.4
 #define OBSTACLE_THRESHOLD 0.1
 #define DECREASE_FACTOR 0.9
@@ -24,14 +25,14 @@
 
 ros::NodeHandle *n;
 
-static int controllerCount;
-static std::vector<std::string> controllerList;
+static int controller_count;
+static std::vector<std::string> controller_list;
 
 ros::ServiceClient time_step_client;
 webots_ros::set_int time_step_srv;
 
-static const char *motorNames[NMOTORS] = {"left_wheel_motor", "right_wheel_motor"};
-static const char *motorNamesComplete[NMOTORS+1] = {"left_wheel_motor", "right_wheel_motor","servo"};
+static const char *motor_names[N_MOTORS] = {"left_wheel_motor", "right_wheel_motor"};
+static const char *motor_names_complete[N_MOTORS+1] = {"left_wheel_motor", "right_wheel_motor","servo"};
 const std::string name = "change";
 const std::string display = "display";
 const std::string path_to_repo = "/Github/Robotics-project";
@@ -41,23 +42,36 @@ double gaussian(double x, double mu, double sigma) {
   return (1.0 / (sigma * sqrt(2.0 * M_PI))) * exp(-((x - mu) * (x - mu)) / (2 * sigma * sigma));
 }
 
-void maxSpeed() {
+void max_speed() {
   // init variables
-  double speeds[NMOTORS];
+  double speeds[N_MOTORS];
 
   // set speeds
-  for (int i = 0; i < NMOTORS; ++i) {
+  for (int i = 0; i < N_MOTORS; ++i) {
     speeds[i] = MAX_SPEED;
     ros::ServiceClient set_velocity_client;
     webots_ros::set_float set_velocity_srv;
-	  set_velocity_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motorNames[i]) + std::string("/set_velocity"));
+	  set_velocity_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motor_names[i]) + std::string("/set_velocity"));
     set_velocity_srv.request.value = speeds[i];
     set_velocity_client.call(set_velocity_srv);
   }
 }
 
+void spin_servo() {
+  // init variables
+  speed = MAX_SPEED;
+
+  // set speeds
+  ros::ServiceClient set_velocity_client;
+  webots_ros::set_float set_velocity_srv;
+  set_velocity_client = n->serviceClient<webots_ros::set_float>(name + std::string("/servo/set_velocity"));
+  set_velocity_srv.request.value = speed;
+  set_velocity_client.call(set_velocity_srv);
+
+}
+
 // Show on the display an image from filesystem
-void imageLoad(const std::string imageName) {
+void image_load(const std::string imageName) {
   ros::ServiceClient display_image_load_client;
   webots_ros::display_image_load display_image_load_srv;
   display_image_load_client = n->serviceClient<webots_ros::display_image_load>(name + "/display/image_load");
@@ -83,7 +97,7 @@ void imageLoad(const std::string imageName) {
 }
 
 // play a .mp3 from filesystem with volume between 0.0 and 1.0 
-void playSound(const std::string soundName, double volume, bool loop) {
+void play_sound(const std::string soundName, double volume, bool loop) {
   ros::ServiceClient speaker_play_sound_client;
   webots_ros::speaker_play_sound speaker_play_sound_srv;
   speaker_play_sound_client = n->serviceClient<webots_ros::speaker_play_sound>(name + "/speaker/play_sound");
@@ -124,14 +138,15 @@ void speak(const std::string &text, double volume) {
 
 
 // Set speaker's language
-// 0 for American English.
-// 1 for British English.
+// 0 for Italian.
+// 1 for American English.
 // 2 for German.
 // 3 for Spanish.
 // 4 for French.
-// 5 for Italian.
-void setLanguage(const int language) {
-  const std::string languages[6] = {"en-US", "en-UK", "de-DE", "es-ES", "fr-FR", "it-IT"};
+// 5 for British English.
+
+void set_language(const int language) {
+  const std::string languages[N_LANGUAGES] = {"it-IT", "en-US", "de-DE", "es-ES", "fr-FR", "en-UK"};
   ros::ServiceClient speaker_set_language_client;
   webots_ros::set_string speaker_set_language_srv;
   speaker_set_language_client = n->serviceClient<webots_ros::set_string>(name + "/speaker/set_language");
@@ -147,11 +162,18 @@ void setLanguage(const int language) {
   time_step_client.call(time_step_srv);
 }
 
+void speak_polyglot(const std::vector<std::string> text, double volume) {
+  for (int i = 0; i < text.size(); ++i) {
+    set_language(i);
+    speak(text[i]+"       ", volume);
+  }  
+}
+
 // catch names of the controllers availables on ROS network
-void controllerNameCallback(const std_msgs::String::ConstPtr &name) {
-  controllerCount++;
-  controllerList.push_back(name->data);
-  ROS_INFO("Controller #%d: %s.", controllerCount, controllerList.back().c_str());
+void controller_name_callback(const std_msgs::String::ConstPtr &name) {
+  controller_count++;
+  controller_list.push_back(name->data);
+  ROS_INFO("Controller #%d: %s.", controller_count, controller_list.back().c_str());
 }
 
 void quit(int sig) {
@@ -171,8 +193,8 @@ int main(int argc, char **argv) {
   signal(SIGINT, quit);
 
   // subscribe to the topic model_name to get the list of availables controllers
-  ros::Subscriber nameSub = n->subscribe("model_name", 100, controllerNameCallback);
-  while (controllerCount == 0 || controllerCount < nameSub.getNumPublishers()) {
+  ros::Subscriber nameSub = n->subscribe("model_name", 100, controller_name_callback);
+  while (controller_count == 0 || controller_count < nameSub.getNumPublishers()) {
     ros::spinOnce();
     ros::spinOnce();
     ros::spinOnce();
@@ -183,14 +205,14 @@ int main(int argc, char **argv) {
   time_step_srv.request.value = TIME_STEP;
 
   // if there is more than one controller available, it let the user choose
-  if (controllerCount == 1)
-    controllerName = controllerList[0];
+  if (controller_count == 1)
+    controllerName = controller_list[0];
   else {
     int wantedController = 0;
     std::cout << "Choose the # of the controller you want to use:\n";
     std::cin >> wantedController;
-    if (1 <= wantedController && wantedController <= controllerCount)
-      controllerName = controllerList[wantedController - 1];
+    if (1 <= wantedController && wantedController <= controller_count)
+      controllerName = controller_list[wantedController - 1];
     else {
       ROS_ERROR("Invalid number for controller choice.");
       return 1;
@@ -201,32 +223,32 @@ int main(int argc, char **argv) {
   nameSub.shutdown();
 
   // init motors
-  for (int i = 0; i < NMOTORS+1; ++i) {
+  for (int i = 0; i < N_MOTORS+1; ++i) {
     // position
     ros::ServiceClient set_position_client;
     webots_ros::set_float set_position_srv;
-    set_position_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motorNamesComplete[i]) +
+    set_position_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motor_names_complete[i]) +
                                                                   std::string("/set_position"));
 
     set_position_srv.request.value = INFINITY;
     
     
     if (set_position_client.call(set_position_srv) && set_position_srv.response.success)
-      ROS_INFO("Position set to INFINITY for motor %s.", motorNamesComplete[i]);
+      ROS_INFO("Position set to INFINITY for motor %s.", motor_names_complete[i]);
     else
-      ROS_ERROR("Failed to call service set_position on motor %s.", motorNamesComplete[i]);
+      ROS_ERROR("Failed to call service set_position on motor %s.", motor_names_complete[i]);
 
     // speed
     ros::ServiceClient set_velocity_client;
     webots_ros::set_float set_velocity_srv;
-    set_velocity_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motorNamesComplete[i]) +
+    set_velocity_client = n->serviceClient<webots_ros::set_float>(name + std::string("/") + std::string(motor_names_complete[i]) +
                                                                   std::string("/set_velocity"));
 
     set_velocity_srv.request.value = 0.0;
     if (set_velocity_client.call(set_velocity_srv) && set_velocity_srv.response.success == 1)
-      ROS_INFO("Velocity set to 0.0 for motor %s.", motorNamesComplete[i]);
+      ROS_INFO("Velocity set to 0.0 for motor %s.", motor_names_complete[i]);
     else
-      ROS_ERROR("Failed to call service set_velocity on motor %s.", motorNamesComplete[i]);
+      ROS_ERROR("Failed to call service set_velocity on motor %s.", motor_names_complete[i]);
   }
 
   
@@ -259,11 +281,11 @@ int main(int argc, char **argv) {
   ROS_INFO("You can now visualize the sensors output in rqt using 'rqt'.");
 
   // for test
-  maxSpeed();
-  imageLoad("warning");
-  playSound("warning",1.0,0);
-  setLanguage(5);
-  speak("ciao come va tutto bene?",1.0);
+  max_speed();
+  spin_servo()
+  image_load("warning");
+  std::vector<std::string> ciao {"Ciao", "Hello", "Halo", "Hola", "Salut"};
+  speak_polyglot(ciao, 1.0);
   // main loop
   while (ros::ok()) {
     if (!time_step_client.call(time_step_srv) || !time_step_srv.response.success) {
