78ddbd0063fd13302b6e4b71f23b4fde1746a0ba
Andrea-Augello <andrea.augello01@community.unipa.it>
2020-12-17 16:20:39 +0100

    retabbed, added some movement primitives

--- catkin_ws/src/webots_ros/scripts/change_main.py
+++ catkin_ws/src/webots_ros/scripts/change_main.py
@@ -8,23 +8,23 @@ import rosservice
 
 
 def testing():
-	call_service('wheel_left_joint','set_velocity',5.0)
-	load_image('warning')
-	call_service('speaker', 'set_language', 'it-IT')
-	speak("Ciao sono ciangà e sugnu troppu fuoitti")
-	speak_polyglot(it_IT="ciao", en_UK="Hello")
+    set_linear_velocity(3.0)
+    load_image('warning')
+    call_service('speaker', 'set_language', 'it-IT')
+    speak("Ciao sono ciangà e sugnu troppu fuoitti")
+    speak_polyglot(it_IT="ciao", en_UK="Hello")
 
 def main():
-	if not rospy.is_shutdown():
-		rospy.init_node(model_name, anonymous=True)
-		rospy.loginfo('Initializing ROS: connecting to ' + os.environ['ROS_MASTER_URI'])
-		rospy.loginfo('Time step: ' + str(time_step))
-		motor_init()
-		enable_sensors()
-		testing()
+    if not rospy.is_shutdown():
+        rospy.init_node(model_name, anonymous=True)
+        rospy.loginfo('Initializing ROS: connecting to ' + os.environ['ROS_MASTER_URI'])
+        rospy.loginfo('Time step: ' + str(time_step))
+        motor_init()
+        enable_sensors()
+        testing()
 
-		
+                
 
 if __name__ == "__main__":  
-	main()    
+    main()    
 
---
+++ catkin_ws/src/webots_ros/scripts/movement_primitives.py
@@ -0,0 +1,69 @@
+from ros_interface import *
+import cv2
+
+angular_velocity = 3.0
+linear_velocity  = 3.0
+
+def set_angular_velocity(speed):
+    call_service(motors[0], 'set_velocity',-speed*330/2);
+    call_service(motors[1], 'set_velocity', speed*330/2);
+    
+
+def set_linear_velocity(speed):
+    call_service(motors[0], 'set_velocity', speed);
+    call_service(motors[1], 'set_velocity', speed);
+
+def stop()
+    set_linear_velocity(0)
+
+def rotate(rotation, precision)
+    """
+    :rotation:  The desired rotation in degrees. Note that a rotation greater
+        than 180° in modulo will be substituted with a rotation in the opposite
+    direction.
+    :precision: The     maximum difference from the required rotation and the
+        actual rotation. If this value is smaller than the sensor noise it makes
+        no sense.
+    """
+        stop()
+        double curr_angular_velocity = angular_velocity
+        direction, framenum=1
+        processCallbacks()
+        #update_frame()
+        #update_object_roi()
+        current_angle = get_angle()
+        target_angle=rotation+get_angle()
+        # adjust for discontinuity at +/-180°
+        target_angle = target_angle >  180 ? target_angle-360 : target_angle
+        target_angle = target_angle < -180 ? 360+target_angle : target_angle
+        difference = target_angle-current_angle
+        if(difference > 180)
+                difference = difference - 360
+         elif (difference < -180)
+                difference = difference + 360
+
+        direction = (difference > 0 ? 1 : -1)
+        while(abs(difference)>precision)
+                #update_frame()
+                framenum = (framenum+1)%100
+                if(!framenum)
+                        if(cv2.waitKey(1) == ' ')
+                                break
+
+                current_angle = get_angle()
+                difference = target_angle-current_angle
+                if(difference > 180)
+                        difference = difference - 360
+                 elif (difference < -180)
+                        difference = difference + 360
+
+                if(direction*difference < 0)
+                # if we went over the specified angle, reverses the direction and
+                # decreases the angular speed to have a better chance of sampling
+                # at the right moment.
+                    curr_angular_velocity*=0.8
+                    direction=-direction
+
+                set_angular_velocity(curr_angular_velocity*(-direction))
+        stop()
+
