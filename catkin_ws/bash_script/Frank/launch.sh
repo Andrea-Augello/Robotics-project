9920c108a13c3057ed6dea15968fba4fadf3c561
Marco <marco.lamartina@community.unipa.it>
2020-12-18 15:47:21 +0100

    merge script removed

--- catkin_ws/bash_script/Frank/launch.sh
+++ catkin_ws/bash_script/Frank/launch.sh
@@ -2,8 +2,4 @@
 
 cd ~/git/Robotics-project/catkin_ws;
 source devel/setup.bash;
-cd src/webots_ros/scripts;
-cat ros_interface.py  > change.py;
-cat movement_primitives.py  >> change.py;
-cat change_main.py  >> change.py;
 roslaunch webots_ros change.launch;
