aa866d29e99d464f2746ce58f210ba469ea6e8e1
Marco <marco.lamartina@community.unipa.it>
2020-12-22 00:12:25 +0100

    tablet message, world restored, head movement, print_info

--- catkin_ws/src/webots_ros/scripts/change.py
+++ catkin_ws/src/webots_ros/scripts/change.py
@@ -8,20 +8,13 @@ from change_pkg.vision import *
 from change_pkg.robot import Change
 
 def testing(robot):
-    robot.tablet.display.load_image('warning')
-    robot.motors.head_vertical.set_position(0)
-    robot.motors.head_vertical.set_velocity(0.5)
-    robot.motors.head_horizontal.set_position(0)
-    robot.motors.head_horizontal.set_velocity(0.5)
-    robot.set_height(robot.motors.torso.max_height/2)
+    robot.tablet.warning()
     robot.movement.scan()
     robot.vision.locate_targets()
-    rospy.logerr(robot.odometry)
     robot.movement.move_forward(2)
     robot.movement.rotate(180)
-    rospy.logerr(robot.odometry)
-    robot.set_height(0)
-    robot.tablet.speaker.speak_polyglot(it_IT="Ciao sono ciangà e sugnu troppu fuoitti", en_UK="Hello I'm ciangà and I'm too strong")
+    robot.print_info()
+
   
         
 def main():
