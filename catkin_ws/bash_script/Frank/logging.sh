2e7741e9e2d8b9670f28cade6c80072a377ac713
Marco La Martina <marco.lamartina@community.unipa.it>
2021-03-11 16:05:28 +0100

    batch mode

--- catkin_ws/bash_script/Frank/logging.sh
+++ catkin_ws/bash_script/Frank/logging.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for i in {1..100}
 do
-   webots --mode=run ~/git/Robotics-project/catkin_ws/src/webots_ros/worlds/change_benchmark_offline.wbt
+   webots --mode=realtime --batch ~/git/Robotics-project/catkin_ws/src/webots_ros/worlds/change_benchmark_offline.wbt
 done
 
